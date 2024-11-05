1936ae797e4afb4e634632b0524cceb84bb0bd05
NATTHANON SANGJAN <66130537@dpu.ac.th>
2024-11-06 00:17:08 +0700

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 // src/services/api.ts
-const API_URL = 'localhost:3000/graphql';
+const API_URL = process.env.BACKEND_URL || 'http://localhost:3000/graphql';
 
 export interface Score {
   id: string;
